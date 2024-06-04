1d82401311299ea29e1e7d75519891f2e14cd443
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-06-04 21:17:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "10000844"))
 API_HASH = getenv("API_HASH", "776f257fc1d1f8aa4aea9dd35d10a45b")
-BOT_TOKEN = getenv("BOT_TOKEN", "6998167875:AAF6c7Y0cI2XydqWGphthDkZNjr9TwdDnbU")
+BOT_TOKEN = getenv("BOT_TOKEN", "7025582687:AAGv9iGPm2xj-R0axV0rri3z1WsKvZpbe-Q")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5638987940 5019308664").split()))
 
 # --------------- Channel ------------ #
