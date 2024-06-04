a91fffee34cab0f07a4c1ecd0637a580fef81a68
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-06-04 22:20:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import getenv
 API_ID = int(getenv("API_ID", "10000844"))
 API_HASH = getenv("API_HASH", "776f257fc1d1f8aa4aea9dd35d10a45b")
 BOT_TOKEN = getenv("BOT_TOKEN", "7025582687:AAGv9iGPm2xj-R0axV0rri3z1WsKvZpbe-Q")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5638987940 6962753370 5019308664").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6750546542 6962753370 5019308664").split()))
 
 # --------------- Channel ------------ #
 FORWARD_IDS = -1002032466369
